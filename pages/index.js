4be1451f7be487743171dc50b6e992278c08ffb0
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2021-07-31 22:21:30 -0700

    Improved form

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,54 @@
 import Head from 'next/head'
 import Image from 'next/Image'
 
+function Form(){
+  return (
+
+    <div className="p-6 card bordered bg-base-100">
+      <div className="form-control">
+        <label className="cursor-pointer label">
+          <span className="label-text">Health Equity for People of Color</span> 
+          <input type="radio" name="cause" className="radio radio-primary" value="health"></input>
+        </label>
+      </div> 
+      <div className="form-control">
+        <label className="cursor-pointer label">
+          <span className="label-text">Immigrants in Need</span> 
+          <input type="radio" name="cause" className="radio radio-primary" value="immigrants"></input>
+        </label>
+      </div> 
+      <div className="form-control">
+        <label className="cursor-pointer label">
+          <span className="label-text">Students in Need</span> 
+          <input type="radio" name="cause" className="radio radio-primary" value="students"></input>
+        </label>
+      </div>    
+
+      <select className="select select-bordered select-info w-full max-w-xs text-blue-700">
+        <option disabled="disabled" selected="selected">Choose your donation amount</option> 
+        <option>$75</option> 
+        <option>$125</option> 
+        <option>$250</option> 
+        <option>$500</option> 
+        <option>$1000</option> 
+        <option>$5000</option> 
+      </select>    
+
+      <div className="divider"></div> 
+
+      <div className="form-control">
+        <label className="cursor-pointer label">
+          <span className="label-text">Monthly Recurring?</span> 
+          <input type="checkbox" className="checkbox checkbox-primary"></input>
+        </label>
+      </div>  
+
+      <button className="btn btn-primary">Donate</button>
+    </div>    
+  )
+}
+
+
 export default function Home() {
   return (
     <div className="md:container md:mx-auto">
@@ -10,7 +58,7 @@ export default function Home() {
       </Head>
 
       {/* <!-- Hero --> */}
-      <div className="hero min-h-screen" style={{backgroundImage:"url(" + "/images/1400.jpg" + ")"}}>
+      <div className="hero min-h-screen" style={{backgroundImage:"url(" + "/images/marguerite_960_720.webp" + ")"}}>
         <div className="hero-overlay bg-opacity-60"></div> 
         <div className="text-center hero-content text-neutral-content">
           <div className="max-w-md">
@@ -20,7 +68,8 @@ export default function Home() {
             <p className="mb-5">
             Kindness First. Margarita Humanitarian Foundation helps those who need help the most. 
                 </p> 
-            <a className="btn btn-primary">Donate</a>
+            {/* <a className="btn btn-primary">Donate</a> */}
+            <Form></Form>
           </div>
         </div>
       </div>
