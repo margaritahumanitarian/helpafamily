94f12f29dfec7aa0251fd659369df32fb5f1349a
Audrey Feldroy <audrey@feldroy.com>
2021-08-05 09:11:03 -0700

    Invite donors to contribute GitHub issues and PRs

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -12,6 +12,7 @@ export default function Home() {
     <p>Your donation is fully tax-deductible. We'll email you a fully-compliant IRS 501(c)(3) donation receipt for your records shortly.</p>
     <h2>About Our Donation Software</h2>
     <p>This page was put together thanks to the kindness of our volunteers. It's still a work in progress, so pardon the dust as we get it working. Today (August 5, 2021) is our first day using it, and we know this page doesn't look quite right yet. We're working on fixing it over the next week. Thanks for your patience and understanding.</p>
+    <p>This donation system is entirely open source at https://github.com/margaritahumanitarian/helpafamily. We're looking for volunteer contributors to help make it better. Report bugs or propose ideas for improvements by submitting GitHub issues, or even submit little pull requests as proposals.</p>
   </body>
 </html>
   )
