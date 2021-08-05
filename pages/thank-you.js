ef547cc552fa3347fedf92c1b15aa17de6018e44
Audrey Feldroy <audrey@feldroy.com>
2021-08-05 09:08:18 -0700

    Fill out text on Thank You page

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -3,10 +3,15 @@ export default function Home() {
 
 <html>
   <head>
-    <title>Help people out</title>
+    <title>Thank You</title>
   </head>
   <body>
-    <p>Thank you for your donation!</p>
+    <h1>Thank You</h1>
+    <p>Thanks so much for your generosity. We promise to use it to help the causes you indicated your support for, and to be extra thoughtful about putting it to good use.</p>
+    <h2>Receipt to Follow</h2>
+    <p>Your donation is fully tax-deductible. We'll email you a fully-compliant IRS 501(c)(3) donation receipt for your records shortly.</p>
+    <h2>About Our Donation Software</h2>
+    <p>This page was put together thanks to the kindness of our volunteers. It's still a work in progress, so pardon the dust as we get it working. Today (August 5, 2021) is our first day using it, and we know this page doesn't look quite right yet. We're working on fixing it over the next week. Thanks for your patience and understanding.</p>
   </body>
 </html>
   )
