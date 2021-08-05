95b7491e9a269c2dbbf2caccfde1724ae290c2be
Audrey Feldroy <audrey@feldroy.com>
2021-08-05 09:18:56 -0700

    Improve Thank You text and its Daisy UI formatting

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -1,19 +1,41 @@
 export default function Home() {
   return (
-
-<html>
-  <head>
-    <title>Thank You</title>
-  </head>
-  <body>
-    <h1>Thank You</h1>
-    <p>Thanks so much for your generosity. We promise to use it to help the causes you indicated your support for, and to be extra thoughtful about putting it to good use.</p>
-    <h2>Receipt to Follow</h2>
-    <p>Your donation is fully tax-deductible. We'll email you a fully-compliant IRS 501(c)(3) donation receipt for your records shortly.</p>
-    <h2>About Our Donation Software</h2>
-    <p>This page was put together thanks to the kindness of our volunteers. It's still a work in progress, so pardon the dust as we get it working. Today (August 5, 2021) is our first day using it, and we know this page doesn't look quite right yet. We're working on fixing it over the next week. Thanks for your patience and understanding.</p>
-    <p>This donation system is entirely open source at https://github.com/margaritahumanitarian/helpafamily. We're looking for volunteer contributors to help make it better. Report bugs or propose ideas for improvements by submitting GitHub issues, or even submit little pull requests as proposals.</p>
-  </body>
-</html>
-  )
+    <html>
+      <head>
+        <title>Thank You</title>
+      </head>
+      <body>
+        <div class="hero min-h-screen bg-base-200">
+          <div class="text-center hero-content">
+            <div class="max-w-md">
+              <h1 class="mb-5 text-5xl font-bold">Thank You</h1>
+              <p>
+                Thanks so much for your generosity. We promise to use it to help
+                the causes you indicated your support for, and to be extra
+                thoughtful about putting it to good use.
+              </p>
+              <h2 class="m-5 text-3xl font-bold">Receipt to Follow</h2>
+              <p>
+                Your donation is fully tax-deductible. We'll email you a
+                fully-compliant IRS 501(c)(3) donation receipt for your records
+                shortly.
+              </p>
+              <h2 class="m-5 text-3xl font-bold">About Our Donation Software</h2>
+              <p>
+                This page was put together thanks to the kindness of our
+                volunteers. It went live on August 5, 2021, so pardon the dust if you discover any problems. Thanks for your patience and understanding.
+              </p>
+              <h3 class="m-5 text-xl font-bold">Improve our Donation Software</h3>
+              <p>
+                This donation system is entirely open source on <a class="link link-secondary" href="https://github.com/margaritahumanitarian/helpafamily">GitHub</a>. We're
+                looking for volunteer contributors to help make it better.
+                Report bugs or propose ideas for improvements by submitting
+                GitHub issues, or even submit little pull requests as proposals.
+              </p>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
 }
