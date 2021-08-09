4528540553f0f2eae9123a1268f180c565edf797
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2021-08-08 20:25:52 -0700

    Prettify index and improve page title

--- pages/index.js
+++ pages/index.js
@@ -1,90 +1,109 @@
-import Head from 'next/head'
-import React, { useState } from 'react';
-import { useRouter } from 'next/router'
+import Head from "next/head";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 
-
-
-function Form(){
-  const router = useRouter()
+function Form() {
+  const router = useRouter();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     let succeed = true;
 
-    if (event.target.cause.value.trim().length === 0){
-      alert("Please choose a cause to support.")
+    if (event.target.cause.value.trim().length === 0) {
+      alert("Please choose a cause to support.");
       succeed = false;
     }
 
-    if (event.target.amount.value == 0){
-      alert("Please choose an amount to give")
+    if (event.target.amount.value == 0) {
+      alert("Please choose an amount to give");
       succeed = false;
-    }    
+    }
 
-    if (succeed){
-      const response = await fetch('/api/create-caring-session', {
+    if (succeed) {
+      const response = await fetch("/api/create-caring-session", {
         body: JSON.stringify({
           amount: event.target.amount.value * 100,
-          cause: event.target.cause.value
+          cause: event.target.cause.value,
         }),
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        method: 'POST'
-      })     
-      const result = await response.json() 
-      router.push(result.url)
+        method: "POST",
+      });
+      const result = await response.json();
+      router.push(result.url);
     }
-    
-
-  }  
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="p-6 card bordered bg-base-100">
         <div className="form-control">
           <label className="cursor-pointer label">
-            <span className="label-text">Students in Need</span> 
-            <input type="radio" name="cause" className="radio radio-primary" value="Students in Need"></input>
+            <span className="label-text">Students in Need</span>
+            <input
+              type="radio"
+              name="cause"
+              className="radio radio-primary"
+              value="Students in Need"
+            ></input>
           </label>
-        </div>    
+        </div>
 
         <div className="form-control">
           <label className="cursor-pointer label">
-            <span className="label-text">People of Color in Need</span> 
-            <input type="radio" name="cause" className="radio radio-primary" value="People of Color in Need"
+            <span className="label-text">People of Color in Need</span>
+            <input
+              type="radio"
+              name="cause"
+              className="radio radio-primary"
+              value="People of Color in Need"
             ></input>
-          </label>
-        </div> 
-        <div className="form-control">
-          <label className="cursor-pointer label">
-            <span className="label-text">Immigrants in Need</span> 
-            <input type="radio" name="cause" className="radio radio-primary" value="Immigrants in Need"></input>
           </label>
         </div>
         <div className="form-control">
           <label className="cursor-pointer label">
-            <span className="label-text">Seniors in Need</span> 
-            <input type="radio" name="cause" className="radio radio-primary" value="Seniors in Need"></input>
+            <span className="label-text">Immigrants in Need</span>
+            <input
+              type="radio"
+              name="cause"
+              className="radio radio-primary"
+              value="Immigrants in Need"
+            ></input>
           </label>
-        </div> 
+        </div>
+        <div className="form-control">
+          <label className="cursor-pointer label">
+            <span className="label-text">Seniors in Need</span>
+            <input
+              type="radio"
+              name="cause"
+              className="radio radio-primary"
+              value="Seniors in Need"
+            ></input>
+          </label>
+        </div>
 
-
-        <select name="amount" className="select select-bordered select-info w-full max-w-xs text-blue-700">
-          <option value="0" disabled="disabled" selected="selected">Choose your donation amount</option> 
-          <option value="50">$50</option> 
-          <option value="75">$75</option> 
+        <select
+          name="amount"
+          className="select select-bordered select-info w-full max-w-xs text-blue-700"
+        >
+          <option value="0" disabled="disabled" selected="selected">
+            Choose your donation amount
+          </option>
+          <option value="50">$50</option>
+          <option value="75">$75</option>
           <option value="125">$125</option>
-          <option value="250">$250</option> 
-          <option value="500">$500</option> 
-          <option value="1000">$1,000</option> 
-          <option value="5000">$5,000</option> 
-          <option value="10000">$10,000</option> 
-          <option value="25000">$25,000</option> 
-        </select>    
+          <option value="250">$250</option>
+          <option value="500">$500</option>
+          <option value="1000">$1,000</option>
+          <option value="5000">$5,000</option>
+          <option value="10000">$10,000</option>
+          <option value="25000">$25,000</option>
+        </select>
 
-        <div className="divider"></div> 
+        <div className="divider"></div>
 
         {/* <div className="form-control">
           <label className="cursor-pointer label">
@@ -93,45 +112,42 @@ function Form(){
           </label>
         </div>   */}
 
-        <button className="btn btn-primary">Donate <span></span></button>
-      </div>   
-    </form> 
-  )
+        <button className="btn btn-primary">
+          Donate <span></span>
+        </button>
+      </div>
+    </form>
+  );
 }
-
 
 export default function Home() {
   return (
     <div className="md:container md:mx-auto">
       <Head>
-        <title>Donate to Margarita Humanitarian
-           Foundation</title>
+        <title>Donate to Margarita Humanitarian Foundation</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {/* <!-- Hero --> */}
-      <div className="hero min-h-screen" style={{backgroundImage:"url(" + "/images/marguerite_960_720.webp" + ")"}}>
-        <div className="hero-overlay bg-opacity-60"></div> 
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url(" + "/images/marguerite_960_720.webp" + ")",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
         <div className="text-center hero-content text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">
-                  Donate
-                </h1> 
+            <h1 className="mb-5 text-4xl font-bold">Help Families in Need</h1>
             <p className="mb-5">
-            Kindness First. Margarita Humanitarian Foundation helps those who need help the most. 
-                </p> 
+              Kindness First. Margarita Humanitarian Foundation helps those who
+              need help the most.
+            </p>
             {/* <a className="btn btn-primary">Donate</a> */}
             <Form></Form>
           </div>
         </div>
       </div>
-
-
-
-
-
-
-
     </div>
-  )
+  );
 }
