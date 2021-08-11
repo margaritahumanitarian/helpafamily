57b8e8606d3b485243d5032512c3f38859d8b158
Sukhamjot-Singh <sukhamjot2001@gmail.com>
2021-08-11 19:27:54 +0530

    Added text to hygienekitsCard and price

--- pages/HygieneKitsCard.js
+++ pages/HygieneKitsCard.js
@@ -3,10 +3,22 @@ function HygieneKitsCard() {
     <div className="card lg:card-side bordered">
       <div className="card-body">
         <h2 className="card-title">Hygiene Kits</h2> 
-        <p className="mb-3">Order a bulk pack of hygiene kits for us to share with people experiencing homelessness.</p> 
-        <p>
-          <a class="btn" href="https://www.amazon.com/96-Kits-Hygiene-Travel-Charity/dp/B0881YKMBL">96 Hygiene Kits</a> 
-        </p>
+        <p className="mb-3">
+            Order a bulk pack of hygiene kits for us to share with people
+            experiencing homelessness.
+            <br></br>
+            Each hygiene kit allows an unhoused individual to freshen up and
+            feel more confident about their hygiene for work, school, or even
+            just to have a better day.
+          </p>
+          <p>
+            <a
+              class="btn btn-primary"
+              href="https://www.amazon.com/96-Kits-Hygiene-Travel-Charity/dp/B0881YKMBL"
+            >
+              96 Hygiene Kits for 153$
+            </a>
+          </p>
         <div class="divider"></div> 
         <h3>Ship the kits to:</h3>
         <p>1543 E Palmdale Blvd, Ste E, Palmdale, CA 93550</p>
