a08e9c4b06e698f1d2a8aa65c676af875b2ec0c3
Marek Rozmus <marek.rozmus@outlook.com>
2021-08-17 15:33:12 +0200

    Fix proptypes issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.func,
+  pageProps: PropTypes.object
+};
 
 export default MyApp;
