7adc75b7bd6c29749aded0427d65d920f34f8697
rathijitpapon <alienpapon009@gmail.com>
2021-08-17 20:31:47 +0600

    :zap: Improve Anyone In Need & Other Checkbox Functionality

--- components/MainDonationForm.js
+++ components/MainDonationForm.js
@@ -6,40 +6,58 @@ export default function MainDonationForm() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    let succeed = true;
     
-    if (event.target.amount.value == 0) {
+    if (event.target.amount.value === 0) {
       alert("Please choose an amount to give");
-      succeed = false;
+      return;
     }
 
-    if (succeed) {
-      const causes = event.target.cause;
-      const validCause = [];
-      for (let i = 0; i < causes.length; i++) {
-        if (causes[i].checked) {
-          validCause.push(" " + causes[i].defaultValue);
-        }
+    const selectedCauses = [];
+    for (let i = 0; i < causes.length; i++) {
+      if (causes[i].isChecked) {
+        selectedCauses.push(" " + causes[i].text);
       }
-      
-      const response = await fetch("/api/create-stripe-session", {
-        body: JSON.stringify({
-          amount: event.target.amount.value * 100,
-          cause: validCause,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      });
-      const result = await response.json();
-      router.push(result.url);
     }
+
+    if (selectedCauses.length === 0) {
+      alert("Please select a cause for donation");
+      return;
+    }
+    
+    const response = await fetch("/api/create-stripe-session", {
+      body: JSON.stringify({
+        amount: event.target.amount.value * 100,
+        cause: selectedCauses,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+    const result = await response.json();
+    router.push(result.url);
   };
 
   // isAnyoneInNeedToggled = stores the state of the "Anyone in need" toggle
   const [isAnyoneInNeedToggled, setIsAnyoneInNeedToggled] = React.useState(true);
+  const [causes, setCauses] = React.useState([
+    {
+      text: "Students in Need",
+      isChecked: false,
+    },
+    {
+      text: "People of Color in Need",
+      isChecked: false,
+    },
+    {
+      text: "Immigrants in Need",
+      isChecked: false,
+    },
+    {
+      text: "Seniors in Need",
+      isChecked: false,
+    },
+  ]);
 
   // mainForm is the topmost form that is shown to the user
   const mainForm = React.createRef();
@@ -52,13 +70,13 @@ export default function MainDonationForm() {
     // If the "Anyone in need" toggle is on, we need to uncheck all the 
     // checkboxes in the cause list
     if (isAnyoneInNeedToggled) {
-      const form = mainForm.current;
+      const data = causes;
 
       // Loop through all the checkboxes in the cause list and uncheck them
-      const inputs = form.getElementsByClassName("donate-form-checkbox");
-      for (let i = 0; i < inputs.length; i++) {
-        inputs[i].checked = false;
+      for (let i = 0; i < data.length; i++) {
+        data[i].isChecked = isAnyoneInNeedToggled;
       }
+      setCauses(data);
     }
   };
 
@@ -80,66 +98,39 @@ export default function MainDonationForm() {
       />
     </label>
   </div>
-  <div className="form-control">
-    <label className="cursor-pointer label">
-      <span className="label-text">Students in Need</span>
-      <input
-        type="checkbox"
-        name="cause"
-        className="checkbox donate-form-checkbox"
-        value="Students in Need"
 
-        // Prevents the browser from using cached checkbox states.
-        // The use of cached values caused interaction issues that were
-        // unintended.
-        autoComplete="off"
+  {
+    causes.map((item, index) => {
+      return (
+        <div className="form-control" key={index}>
+          <label className="cursor-pointer label">
+            <span className="label-text">{item.text}</span>
+            <input
+              type="checkbox"
+              name="cause"
+              className="checkbox donate-form-checkbox"
+              value={item.text}
+              checked={item.isChecked}
+              onChange={() => {
+                const data = [...causes];
+                data[index].isChecked = !data[index].isChecked;
+                setCauses(data);
+              }}
+              // Prevents the browser from using cached checkbox states.
+              // The use of cached values caused interaction issues that were
+              // unintended.
+              autoComplete="off"
 
-        // If the "Anyone in need" toggle is on, we need to uncheck all the
-        // checkboxes in the cause list, such as this one
-        disabled={!isAnyoneInNeedToggled}
-        autoComplete="off"
-      />
-    </label>
-  </div>
-  <div className="form-control">
-    <label className="cursor-pointer label">
-      <span className="label-text">People of Color in Need</span>
-      <input
-        type="checkbox"
-        name="cause"
-        className="checkbox donate-form-checkbox"
-        value="People of Color in Need"
-        disabled={!isAnyoneInNeedToggled}
-        autoComplete="off"
-      />
-    </label>
-  </div>
-  <div className="form-control">
-    <label className="cursor-pointer label">
-      <span className="label-text">Immigrants in Need</span>
-      <input
-        type="checkbox"
-        name="cause"
-        className="checkbox donate-form-checkbox"
-        value="Immigrants in Need"
-        disabled={!isAnyoneInNeedToggled}
-        autoComplete="off"
-      />
-    </label>
-  </div>
-  <div className="form-control">
-    <label className="cursor-pointer label">
-      <span className="label-text">Seniors in Need</span>
-      <input
-        type="checkbox"
-        name="cause"
-        className="checkbox donate-form-checkbox"
-        value="Seniors in Need"
-        disabled={!isAnyoneInNeedToggled}
-        autoComplete="off"
-      />
-    </label>
-  </div>
+              // If the "Anyone in need" toggle is on, we need to uncheck all the
+              // checkboxes in the cause list, such as this one
+              disabled={!isAnyoneInNeedToggled}
+              autoComplete="off"
+            />
+          </label>
+        </div>
+      );
+    })
+  }
 
   <select
     name="amount"
