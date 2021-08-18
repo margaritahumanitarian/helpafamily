97c138efb16d6fbafa55dd7a792e06bbf1c011c0
Anthony Lopez <grungekarma@gmail.com>
2021-08-18 16:59:59 -0500

    update tailwind.config for responsivness && add multi column layout with mobile responsivness

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,51 @@
-import Head from 'next/head';
-import React from 'react';
+import Head from "next/head";
+import React from "react";
 
-import FoodDistributionSupportCard from '../components/FoodDistributionSupportCard';
-import HealthWorkshopCard from '../components/HealthWorkshopCard';
-import HotMealDayCard from '../components/HotMealDayCard';
-import HumanitarianClinicCard from '../components/HumanitarianClinicCard';
-import HygieneKitsCard from '../components/HygieneKitsCard';
-import LaptopForFamiliesCard from '../components/LaptopForFamiliesCard';
-import MainDonationForm from '../components/MainDonationForm';
+import FoodDistributionSupportCard from "../components/FoodDistributionSupportCard";
+import HealthWorkshopCard from "../components/HealthWorkshopCard";
+import HotMealDayCard from "../components/HotMealDayCard";
+import HumanitarianClinicCard from "../components/HumanitarianClinicCard";
+import HygieneKitsCard from "../components/HygieneKitsCard";
+import LaptopForFamiliesCard from "../components/LaptopForFamiliesCard";
+import MainDonationForm from "../components/MainDonationForm";
 
 const ListOfCards = [
   {
     card: <HealthWorkshopCard />,
     amount: 5000,
-    id: 'health workshop',
+    id: "health workshop",
   },
   {
     card: <HygieneKitsCard />,
     amount: 153,
-    id: 'hygiene kits',
+    id: "hygiene kits",
   },
   {
     card: <LaptopForFamiliesCard />,
     amount: 0,
-    id: 'laptop for families',
+    id: "laptop for families",
   },
   {
     card: <HotMealDayCard />,
     amount: 250,
-    id: 'hot meal day',
+    id: "hot meal day",
   },
   {
     card: <FoodDistributionSupportCard />,
     amount: 150,
-    id: 'food distribution support',
+    id: "food distribution support",
   },
   {
     card: <HumanitarianClinicCard />,
     amount: 30,
-    id: 'humanitarian clinic',
+    id: "humanitarian clinic",
   },
 ];
 
 const sortedCards = [...ListOfCards];
 sortedCards.sort((a, b) => a.amount - b.amount);
 
-const Cards = () => 
+const Cards = () =>
   sortedCards.map(({ card, id }) => (
     <div key={id}>
       {card}
@@ -55,9 +55,9 @@ const Cards = () =>
 
 export default function Home() {
   return (
-    <div className="md:container md:mx-auto">
+    <div className="w-screen">
       <Head>
-        <title>{'Donate to Margarita Humanitarian Foundation'}</title>
+        <title>{"Donate to Margarita Humanitarian Foundation"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -65,21 +65,31 @@ export default function Home() {
       <div
         className="hero min-h-screen"
         style={{
-          backgroundImage: 'url(/images/marguerite_960_720.webp)',
+          backgroundImage: "url(/images/marguerite_960_720.webp)",
         }}
       >
         <div className="hero-overlay bg-opacity-60" />
         <div className="text-center hero-content text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-4xl font-bold">{'Help Families in Need'}</h1>
+          <div className="w-lg">
+            <h1 className="mb-5 text-4xl font-bold">
+              {"Help Families in Need"}
+            </h1>
             <p className="mb-5">
-              {'Kindness First. Margarita Humanitarian Foundation helps those who need help the most.'}
+              {
+                "Kindness First. Margarita Humanitarian Foundation helps those who need help the most."
+              }
             </p>
             {/* <a className="btn btn-primary">Donate</a> */}
-            <MainDonationForm />
-            <Cards />
+            <div className="md:flex md:justify-center">
+              <MainDonationForm />
+            </div>
+            <div className="md:grid md:grid-cols-3 md:space-x-4">
+              <Cards />
+            </div>
             <p>
-              {'Margarita Humanitarian Foundation is a 501(c)(3) nonprofit registered in the US.'}
+              {
+                "Margarita Humanitarian Foundation is a 501(c)(3) nonprofit registered in the US."
+              }
             </p>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,19 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    screens: { md: "100%" },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   variants: {
