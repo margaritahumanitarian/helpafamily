c467d056a30c7c59ba06552b127b68c2a58fcdb4
Anthony Lopez <grungekarma@gmail.com>
2021-08-18 17:29:03 -0500

    fix single quote problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  mode: "jit",
+  mode: 'jit',
   purge: {
     content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
     ],
     options: {
       safelist: [/data-theme$/],
@@ -12,16 +12,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      sm: "640px",
+      sm: '640px',
       // => @media (min-width: 640px) { ... }
 
-      md: "768px",
+      md: '768px',
       // => @media (min-width: 768px) { ... }
 
-      lg: "1024px",
+      lg: '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      xl: "1280px",
+      xl: '1280px',
       // => @media (min-width: 1280px) { ... }
     },
     extend: {},
@@ -29,5 +29,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
 };
