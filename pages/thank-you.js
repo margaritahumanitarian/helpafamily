0c53fa2c258541510ab709198e59829817095a83
Audrey Roy Greenfeld <audrey.roy.greenfeld@margaritahumanitarian.org>
2021-08-19 05:49:06 -0700

    Iterate on @shreykoradia's Reach Out link PR

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -29,12 +29,11 @@ export default function Home() {
                 <a className="link link-secondary" href="https://github.com/margaritahumanitarian/helpafamily">{'GitHub'}</a>
                 {'. We\'re looking for volunteer contributors to help make it better. Report bugs or propose ideas for improvements by submitting GitHub issues, or even submit little pull requests as proposals.'}
               </p>
-              <h3 className="m-5 text-xl font-bold">{'Reach out us at'}</h3>
+              <h3 className="m-5 text-xl font-bold">{'Reach Out to Us'}</h3>
               <p>
-                {'Follow Margarita Humanitarian Foundation for more updates '}
+                {'Follow Margarita Humanitarian Foundation for more updates: '}
                 <a className="link link-secondary" href="https://www.instagram.com/margaritahforg/">{'Instagram'}</a> {'and'}
                 <a className="link link-secondary" href="https://twitter.com/margaritahforg">{'Twitter'}</a>
-                
               </p>
             </div>
           </div>
