b8c0d8f56bc91287adc7e9491ebce490fb910c77
Seth Reeise <southsc22@gmail.com>
2021-08-18 22:16:25 -0400

    fix magic numbers

--- pages/api/create-stripe-session.js
+++ pages/api/create-stripe-session.js
@@ -1,3 +1,7 @@
+import {
+  StatusCodes,
+} from 'http-status-codes';
+
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function helloAPI(req, res) {
@@ -24,5 +28,5 @@ export default async function helloAPI(req, res) {
     cancel_url: process.env.CANCEL_URL,
   });
 
-  res.status(200).json({ url: session.url });
+  res.status(StatusCodes.OK).json({ url: session.url });
 }
