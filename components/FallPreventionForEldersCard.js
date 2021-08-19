058626bbd7c9862062fde4a69a1418d4c311b315
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2021-08-18 20:37:02 -0700

    Text improvements to Fall Prevention Class

--- components/FallPreventionForEldersCard.js
+++ components/FallPreventionForEldersCard.js
@@ -10,7 +10,7 @@ function FallPreventionForEldersCard() {
     const response = await fetch('/api/create-stripe-session', {
       body: JSON.stringify({
         amount: 10000,
-        cause: 'Fall prevention class',
+        cause: 'Provide 1 Fall Prevention Class Seat to an Elder',
       }),
       headers: {
         'Content-Type': 'application/json',
@@ -24,14 +24,14 @@ function FallPreventionForEldersCard() {
   return (
     <div className="card lg:card-side bordered">
       <div className="card-body">
-        <h2 className="card-title">{'Fall prevention class'}</h2>
+        <h2 className="card-title">{'Fall Prevention Class'}</h2>
         <p className="mb-3">
           {'Provide an elderly person with a 6 week course in fall prevention while covering costs such as insurance, curriculum development and review.'}
         </p>
         <p>
           <form onSubmit={handleSubmit}>
             <button className="btn btn-primary" type="submit">
-              {'Provide one seat for $100'}
+              {'Provide 1 Seat for $100'}
             </button>
           </form>
         </p>
