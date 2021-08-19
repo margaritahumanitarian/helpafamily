537c09b00af225045096b38bdbd248461d44ba20
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2021-08-18 19:59:01 -0700

    Update health workshop card to address #58 comments

--- components/HealthWorkshopCard.js
+++ components/HealthWorkshopCard.js
@@ -25,17 +25,17 @@ function HealthWorkshopCard() {
         <div className="card-body">
           <h2 className="card-title">Health Education Workshop</h2>
           <p className="mb-3">
-          Helps in establishing One free health education workshop for a community that is facing major disparities in health conditions 
-          and to help those people who need care and attention like elders, young mothers including teen mothers, migrant workers, 
-          low-income families, foster families, unhoused families, and families of incarcerated parents.<br />
-          Also, Your donation will be put in service for health-themed mini gift bags, raffle prizes to motivate people to attend a workshop, 
-          equipment to run the workshop such as trainer simulaids for edema and breast disease, 
-          and to hire public health educators to prepare presentations...
+          Help in establishing one free health education workshop for a community that is facing major disparities in health conditions 
+          and to help those people who need care and attention.</p>
+          <p className="mb-3">
+          Also, your donation will go toward health-themed mini gift bags to motivate people to attend a workshop, 
+          equipment such as trainer simulaids for edema and breast disease, 
+          venue costs, and to hire public health educators to prepare presentations.
           </p>
           <p>
           <form onSubmit={handleSubmit}>
               <button className="btn btn-primary">
-              Help in Creating One health workshop for $5000 <span />
+              Help Create 1 Health Workshop for $5000 <span />
               </button>
             </form>
           </p>
