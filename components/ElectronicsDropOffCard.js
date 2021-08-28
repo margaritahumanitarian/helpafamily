6e8e74f5ae28a63d20789306d2381b017e74533d
Audrey Roy Greenfeld <audrey@feldroy.com>
2021-08-27 19:36:43 -0700

    Update components/ElectronicsDropOffCard.js

--- components/ElectronicsDropOffCard.js
+++ components/ElectronicsDropOffCard.js
@@ -5,7 +5,7 @@ import Card, { CardAddress, CardParagraph, CardTitle } from './Card';
 function ElectronicsDropOffCard() {
   return (
     <Card backgroundImageSource="/images/ElectronicsDrop.jpg">
-      <CardTitle>{'Electronics Drop Off Box'}</CardTitle>
+      <CardTitle>{'Electronics Drop-Off Box'}</CardTitle>
       <CardParagraph>
         {
           "Any organization or business around the world can help families get access to technology. Put a donation box in your office for used laptops, tablets, and smartphones. Volunteers will pick up the items periodically, fix them up, and provide them to families in need. You can optionally be involved with refurbishment and placement too if you'd like."
