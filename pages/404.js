2ed48a89acc954d51224f641968e86df5cb3aee9
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2021-08-29 09:49:56 -0700

    Add a card giving a little more context

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import Card, { CardParagraph, CardTitle } from '../components/Card';
 import PrimaryLayout from '../components/PrimaryLayout';
+
 
 export default function NotFound() {
   return (
     <>
       <PrimaryLayout />
+      <Card backgroundImageSource="/images/Volunteer.jpg">
+        <CardTitle>{'404 Page Not Found'}</CardTitle>
+
+        <CardParagraph>{'Always act in kindness'}</CardParagraph>        
+      </Card>
     </>
   );
 }
