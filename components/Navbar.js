a26cb595d2f4206f6f550b6c7518095e93c0489e
Marek Rozmus <marek.rozmus@outlook.com>
2021-09-01 18:27:46 +0200

    Make navbar sticky

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content justify-center">
+    <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content justify-center sticky top-0 z-50">
       <div className="flex-none px-2 mx-2">
         <span className="text-lg font-bold">
           {'Margarita Humanitarian Foundation'}
