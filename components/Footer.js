e4dc1b4645e9e9a35f12d7943563ea963ca502f3
akshansh <akshanshrewariya@gmail.com>
2021-09-03 23:20:09 +0530

    Added GuideStar Seal of Transparency to the Footer

--- components/Footer.js
+++ components/Footer.js
@@ -9,14 +9,31 @@ export default function Footer() {
           'Margarita Humanitarian Foundation is a 501(c)(3) nonprofit registered in the US.'
         }
       </span>
-      <a href="https://vercel.com?utm_source=margaritahumanitarian&utm_campaign=oss">
-        <Image
-          alt="Powered by Vercel"
-          height="44"
-          src="/images/powered-by-vercel.svg"
-          width="212"
-        />
-      </a>
+      <div className="flex justify-center items-center cursor-pointer">
+        <a
+          className="mr-4 mt-2"
+          href="https://vercel.com?utm_source=margaritahumanitarian&utm_campaign=oss"
+        >
+          <Image
+            alt="Powered by Vercel"
+            height="44"
+            src="/images/powered-by-vercel.svg"
+            width="212"
+          />
+        </a>
+        <a
+          href="https://www.guidestar.org/profile/84-4323038"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <Image
+            alt="GuideStar Seal of Transparency"
+            height="80"
+            src="/images/GuideStarSeal.svg"
+            width="80"
+          />
+        </a>
+      </div>
     </footer>
   );
 }
