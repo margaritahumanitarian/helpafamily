c62df55c8464ebfcf3ea2422bf25ab05cf3d89a7
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2021-09-06 13:04:01 -0700

    Run `yarn format`

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -14,7 +14,9 @@ export default function Home() {
             <div className="text-center hero-content">
               <div className="max-w-md">
                 <h1 className="mb-5 text-5xl font-bold">{'Thank You!'}</h1>
-                <p>{'With gratitude from Margarita Humanitarian Foundation.'}</p>
+                <p>
+                  {'With gratitude from Margarita Humanitarian Foundation.'}
+                </p>
                 <p>
                   {
                     'Thanks so much for your generosity. We promise to use it to help the causes you indicated your support for, and to be extra thoughtful about putting it to good use.'
@@ -27,7 +29,9 @@ export default function Home() {
             <img src="/images/MHF-color-300x300.png" />
           </div>
           <div className="self-center text-center">
-            <h3 className="m-0 mt-10 mb-2 text-3xl font-bold">{'Reach Out to Us'}</h3>
+            <h3 className="m-0 mt-10 mb-2 text-3xl font-bold">
+              {'Reach Out to Us'}
+            </h3>
             <p>
               {'Follow Margarita Humanitarian Foundation for more updates: '}
             </p>
@@ -47,7 +51,9 @@ export default function Home() {
           </div>
           <div className="flex flex-row justify-evenly grid-cols-3 inset-x-0 bottom-0 ">
             <div className="max-w-lg text-center">
-              <h2 className="m-0 mt-10 mb-2 text-3xl font-bold">{'Receipt to Follow'}</h2>
+              <h2 className="m-0 mt-10 mb-2 text-3xl font-bold">
+                {'Receipt to Follow'}
+              </h2>
               <p>
                 {
                   "Your donation is fully tax-deductible. We'll email you a fully-compliant IRS 501(c)(3) donation receipt for your records shortly."
@@ -81,9 +87,7 @@ export default function Home() {
                   ". We're looking for volunteer contributors to help make it better. Report bugs or propose ideas for improvements by submitting GitHub issues, or even submit little pull requests as proposals."
                 }
               </p>
-
             </div>
-
           </div>
           <Footer />
         </div>
