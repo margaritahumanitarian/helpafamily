22864cb5d520574e5b986f071bbb302400dac90d
ozer619 <prathameshd419@gmail.com>
2021-09-10 19:02:39 +0530

    css not working

--- pages/404.js
+++ pages/404.js
@@ -1,23 +1,26 @@
+import Link from 'next/link';
 import React from 'react';
-
-import Card, { CardParagraph, CardTitle } from '../components/Card';
-import PrimaryLayout from '../components/PrimaryLayout';
 
 export default function NotFound() {
   return (
-    <>
-      <PrimaryLayout />
-      <Card backgroundImageSource="/images/Volunteer.jpg">
-        <CardTitle>{'404 Page Not Found'}</CardTitle>
-        <CardParagraph>
-          {
- 'The page you requested was not found. Always act in kindness by reporting errors like this'}
-<Link href="https://github.com/margaritahumanitarian/helpafamily/issues/new/choose"
-{"here"}
-</Link>           
-          }
-        </CardParagraph>
-      </Card>
-    </>
+    <html>
+      <head>
+        <title> {'404 Not Found'} </title>
+      </head>
+      <body>
+        <div className="container mx-auto mx-auto ">
+          <div className="flex flex-wrap items-center justify-center">
+            <div className="lg:w-1/2 pr-4 pl-4 vmb-2 font-medium leading-tight text-4xl00 h-screen">
+              <img alt="gif" src="/images/404.gif"/>
+            </div>
+            <div className=":flex-growlg-6 m-auto h-screen">
+              <h1 className="m-0 mt-10 mb-2 text-3xl font-bold">
+                {'hello world'}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
