7662df779c627c6d0b6012b88a0752b3fb8b76ee
ozer619 <prathameshd419@gmail.com>
2021-09-11 22:18:30 +0530

    Added to link issue page

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,14 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 import ImageGif404 from '../public/images/404.gif';
+
 export default function NotFound() {
   return (
     <html>
       <head>
-        <title> {'404 Not Found'} </title>
+        <title>{'404 Not Found'}</title>
       </head>
       <body>
         <div className="hero min-h-screen bg-base-200 bg-white">
@@ -14,13 +16,23 @@ export default function NotFound() {
             <div className="">
               <div className="flex justify-center items-center">
                 <div className="lg:w-1/2 pr-4 pl-4">
-                  <Image alt="gif" src={ImageGif404} />
+                  <Image alt="404-gif" src={ImageGif404} />
                 </div>
                 <h1 className="mx-auto mb-2 text-3xl font-bold leading-10">
                   {'Oops!!!'}
                   <br />
                   {'404 page not found'}
                 </h1>
+              </div>
+              <div>
+                <p className="text-blue-300">
+                  {
+                    'The page you requested was not found. Always act in kindness by reporting errors like this '
+                  }
+                  <Link href="https://github.com/margaritahumanitarian/helpafamily/issues/new/choose">
+                    <span className="text-blue-700">{'here'}</span>
+                  </Link>
+                </p>
               </div>
             </div>
           </div>
