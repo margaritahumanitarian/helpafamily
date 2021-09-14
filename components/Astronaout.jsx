d432a2486afe78cc271b1a8649255ed8e3e9f58a
ozer619 <prathameshd419@gmail.com>
2021-09-14 17:09:21 +0530

    added home button nad svg animation

---
+++ components/Astronaout.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Lottie from 'react-lottie';
+import animationData from '../ lotties/astronaout.json';
+
+class AstronaoutLottie extends Component {
+  render() {
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice',
+      },
+    };
+
+    return (
+      <div>
+        <Lottie options={defaultOptions} height={400} width={400} />
+      </div>
+    );
+  }
+}
+
+export default AstronaoutLottie;
--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,7 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-import ImageGif404 from '../public/images/404.gif';
+import AstronaoutLottie from '../components/Astronaout';
 
 export default function NotFound() {
   return (
@@ -15,8 +14,8 @@ export default function NotFound() {
           <div className="text-center hero-content">
             <div className="">
               <div className="flex justify-center items-center">
-                <div className="lg:w-1/2 pr-4 pl-4">
-                  <Image alt="Caveman accidentally getting electrocuted" src={ImageGif404} />
+                <div className="lg:w-1/2 pr-10 pl-4">
+                  <AstronaoutLottie />
                 </div>
                 <h1 className="mx-auto mb-2 text-3xl font-bold leading-10">
                   {'Oops!!!'}
@@ -25,14 +24,21 @@ export default function NotFound() {
                 </h1>
               </div>
               <div>
-                <p className="text-blue-300">
+                <p className="text-blue-300 mt-5">
                   {
                     'The page you requested was not found. Always act in kindness by reporting errors like this '
                   }
-                  <Link href="https://github.com/margaritahumanitarian/helpafamily/issues/new/choose" passHref>
-                    <span className="text-blue-700">{'here'}</span>
+                  <Link href="https://github.com/margaritahumanitarian/helpafamily/issues/new/choose">
+                    <span className="text-blue-500">{'here'}</span>
                   </Link>
                 </p>
+              </div>
+              <div>
+                <Link href="/">
+                  <button class="bg-transparent mt-10 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    {'Home'}
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
