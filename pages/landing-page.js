7538c13ca0317726e5c2ea0e8bffca97665bf37a
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2021-09-18 23:28:22 -0700

    Refactored and got rid of all custom styled jsx css. Changed out the button for a next/link on line 289. Made hero section full screen.

--- pages/landing-page.js
+++ pages/landing-page.js
@@ -4,11 +4,22 @@ import { ReactElement, useEffect, useState } from 'react'; // ReactElement is on
 import Footer from '../components/Footer';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
-import { useRouter } from 'next/router';
 
 // import { BsSearch } from 'react-icons/bs';
 import { FaRegHeart } from 'react-icons/fa';
+
+/** -----------------------------------------------------------------------------------------------------------------------
+ *
+ * TODO: Change all of the placeholder text to good copy.
+ * TODO: Change the call to action link to point to the appropriate destination.
+ * TODO: Refactor code by exporting components to proper folders.
+ *
+ * FIXME: The images are placeholders and need to be changed.
+ * FIXME: There's a better way to handle images as per @RedFox0x20 recommendation.
+ *
+ */ // --------------------------------------------------------------------------------------------------------------------
 
 // -----------------------------------------------------------------------------------------------------------------------
 // TODO: Export LargeHeroSection to components folder.
@@ -16,45 +27,28 @@ import { FaRegHeart } from 'react-icons/fa';
 
 /**
  * ## Large Hero Section
- * This component is the main `LargeHeroSection` component.
+ * This component is the main `LargeHeroSection` component hosting the image.
  *
  * ### Modify using tailwind classes or css
  * - `opacity` : "bg-opacity-`opacity`" | Example: `"30"` | [Tailwind opacity](https://tailwindcss.com/docs/background-opacity)
- * - `heroHeight` : Use standard css (vh | px | %) :  Example: `"70vh"`
  *
- * @param {Object} props - bgImage, mainTextSize, opacity, title, titleSize
+ * @param {Object} props - { bgImage, mainTextSize, opacity, title, titleSize }
  * @param {String} props.bgImage - Background image url
- * @param {String} [props.heroHeight = '100vh'] Use standard css (vh | px | %) :  Example: `"70vh"`
- * @param {String} [props.opacity = '60' ]"bg-opacity-`opacity`" | Example: `"30"` | [Tailwind opacity](https://tailwindcss.com/docs/background-opacity)
+ * @param {String} [props.opacity = '60' ] "bg-opacity-`opacity`" | Example: `"30"` | [Tailwind opacity](https://tailwindcss.com/docs/background-opacity)
  * @param {*} props.children - Children elements
  * @returns {ReactElement} JSX
  */
 
-const LargeHeroSection = ({
-  bgImage,
-  children,
-  heroHeight = '100vh',
-  opacity = '60',
-}) => {
+const LargeHeroSection = ({ bgImage, children, opacity = '60' }) => {
   return (
     <>
-      <div className="hero min-height background">
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      >
         <div className={`hero-overlay bg-opacity-${opacity}`} />
         {children}
       </div>
-      <style jsx>{`
-        .background {
-          background-image: url(${bgImage});
-        }
-        .min-height {
-          height: ${heroHeight};
-        }
-        @media (max-width: 768px) {
-          .hero {
-            height: 100vh;
-          }
-        }
-      `}</style>
     </>
   );
 };
@@ -62,7 +56,6 @@ const LargeHeroSection = ({
 LargeHeroSection.propTypes = {
   children: PropTypes.node,
   bgImage: PropTypes.string.isRequired,
-  heroHeight: PropTypes.string,
   opacity: PropTypes.string,
 };
 
@@ -76,15 +69,15 @@ LargeHeroSection.propTypes = {
 
 /**
  * ## Large Hero Content
- * This component goes full width and height of the page by default. Modify with props.
+ * This is the call to action and content of the `LargeHeroSection` component.
  *
  * ### Modify using tailwind classes or css
  * - `titleSize` : "text-`titleSize`" | Example: `"5xl"` | [Tailwind text sizes](https://tailwindcss.com/docs/font-size)
  * - `mainTextSize` : "max-w-`mainSize`" | [Tailwind max-width sizes](https://tailwindcss.com/docs/max-width)
  *
- * @param {Object} props - { mainTextSize, opacity, title, titleSize, fadeInSpeed }
+ * @param {Object} props - { mainTextSize, title, titleSize, fadeInSpeed }
  * @param {String} [props.mainTextSize = 'md'] - change the size of main section "max-w-`mainSize`" | [Tailwind max-width sizes](https://tailwindcss.com/docs/max-width)
- * @param {String} [props.fadeInSpeed = '0.8s'] - change the speed of fade in "ease-in-out-`fadeInSpeed`" | Example: `"0.8s"`
+ * @param {String} [props.fadeInSpeed = '700'] - change the speed of fade in "duration-`fadeInSpeed`" | Example: `700"`
  * @param {String} props.title - The title of the hero section
  * @param {String} [props.titleSize = '5xl'] - "text-`titleSize`" | Example: `"5xl"` | [Tailwind text sizes](https://tailwindcss.com/docs/font-size)
  * @param {*} props.children - The content of the hero section
@@ -93,33 +86,23 @@ LargeHeroSection.propTypes = {
 
 const LargeHeroContent = ({
   children,
-  fadeInSpeed = '0.8s',
+  fadeInSpeed = '700',
   mainTextSize = 'md',
   title,
   titleSize = '5xl',
 }) => {
-  const [fadeIn, setFadeIn] = useState(0);
+  const [opacityAmount, setOpacityAmount] = useState(0);
   useEffect(() => {
-    setFadeIn(1);
+    setOpacityAmount(1);
   }, []);
   return (
-    <div className="text-center hero-content text-neutral-content fade-in">
+    <div
+      className={`text-center hero-content text-neutral-content opacity-${opacityAmount} transition ease-in duration-${fadeInSpeed}`}
+    >
       <div className={`max-w-${mainTextSize}`}>
         <h1 className={`mb-6 text-${titleSize} font-bold`}>{title}</h1>
         {children}
       </div>
-      <style jsx>{`
-        .fade-in {
-          opacity: ${fadeIn};
-          transition: ease-in-out ${fadeInSpeed};
-        }
-        @media (max-width: 768px) {
-          .hero-content {
-            padding-top: 0;
-            height: 100vh;
-          }
-        }
-      `}</style>
     </div>
   );
 };
@@ -138,31 +121,36 @@ LargeHeroContent.propTypes = {
 
 // -----------------------------------------------------------------------------------------------------------------------
 // TODO: Export LandingPageNav to components folder.
+// FIXME: The commented out code is for @Audrey to decide whether or not she wants to use the links in the nav.
 // -----------------------------------------------------------------------------------------------------------------------
 
 /**
  * ### Landing Page Nav Bar
  * This component is used to create the nav bar for the landing page which has opacity and fade in options to work in conjunction with the `LargeHeroSection` component.
- * @param {Object} props - fadeInSpeed
- * @param {String} [props.fadeInSpeed = '1.5s' ]- change the speed of fade in "ease-in-out-`fadeInSpeed`" | Example: `"0.8s"`
+ *
+ * fadeInSpeed - change the speed of fade in "ease-in duration-`fadeInSpeed`" | Example: `"700"` : [tailwind durations](https://tailwindcss.com/docs/transition-duration)
+ *
+ * @param {Object} props - { fadeInSpeed }
+ * @param {String} [props.fadeInSpeed = '700' ] - change the speed of fade in "ease-in duration-`fadeInSpeed`" | Example: `"700"` : [tailwind durations](https://tailwindcss.com/docs/transition-duration)
  * @returns {ReactElement} JSX
  */
-const LandingPageNav = ({ fadeInSpeed = '1.5s' }) => {
+const LandingPageNav = ({ fadeInSpeed = '700' }) => {
   const [navOpacity, setnavOpacity] = useState(0);
   useEffect(() => {
-    setnavOpacity(1);
+    setnavOpacity(100);
   }, []);
   return (
     <>
-      <div className="nav-area">
-        <div className="nav-bar mb-2 shadow-lg bg-neutral text-neutral-content justify-center sticky top-0 z-50">
-          <div className="flex-none px-6 mx-2">
-            <span className="text-lg font-bold">
-              {'Margarita Humanitarian Foundation'}
-            </span>
-          </div>
-          <div className="flex-1 px-2 mx-2 hidden md:flex">
-            {/* <div className="items-stretch hidden lg:flex">
+      <div
+        className={`fixed h-20 nav-bar mb-2 shadow-lg text-neutral-content flex justify-between items-center top-0 left-0 right-0 z-50 opacity-${navOpacity} ease-in duration-${fadeInSpeed}`}
+      >
+        <div className="flex-none px-6 mx-2">
+          <span className="text-lg font-bold">
+            {'Margarita Humanitarian Foundation'}
+          </span>
+        </div>
+        {/* <div className="px-2 mx-2 hidden md:flex flex-grow "> */}
+        {/* <div className="items-stretch hidden lg:flex">
               <Link href="/">
                 <a className="btn btn-ghost btn-sm rounded-btn">{'Home'}</a>
               </Link>
@@ -183,50 +171,53 @@ const LandingPageNav = ({ fadeInSpeed = '1.5s' }) => {
                 </a>
               </Link>
             </div> */}
-          </div>
-          <div className="pr-7 hidden">{' Help Families in Need'}</div>
+        {/* </div> */}
+        <div className="flex items-center">
+          <div className="pr-7 hidden md:block">{' Help Families in Need'}</div>
           <div className="flex-row hidden sm:flex">
             <button className="btn btn-square btn-ghost pr-6" type="button">
               <FaRegHeart className="inline-block w-5 h-5 stroke-current" />
             </button>
           </div>
+        </div>
 
-          {/* <div className="flex-none hidden sm:flex">
+        {/* <div className="flex-none hidden sm:flex">
             <button className="btn btn-square btn-ghost" type="button">
               <BsSearch className="inline-block w-5 h-5 stroke-current" />
             </button>
           </div> */}
-        </div>
       </div>
-      <style jsx>{`
-        .hidden {
-          display: block;
-        }
-        .nav-bar {
-          opacity: ${navOpacity};
-          background-color: #2159633d !important;
-          box-shadow: 0 0.3px 7.2px 0px rgba(0, 0, 0, 0.12);
-          z-index: 1;
-          position: fixed;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          top: ${navOpacity ? navOpacity - 1 : -3}rem;
-          left: 0;
-          right: 0;
-          height: 73px;
-          transition: all ${fadeInSpeed} ease-in-out;
-        }
-        @media (max-width: 768px) {
-          .nav-bar {
-            top: 0;
-          }
-          .hidden {
-            display: none;
-          }
-        }
-      `}</style>
     </>
+  );
+};
+
+// -----------------------------------------------------------------------------------------------------------------------
+// Export LandingPageNav end.
+// -----------------------------------------------------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------------------------------------------------
+// TODO: Export TestimonialCard to components folder or leave it here as it's not the most reusable component.
+// FIXME: Find correct images for these cards and fix the sizing issues
+// -----------------------------------------------------------------------------------------------------------------------
+
+const TestimonialCard = ({ content, image, title }) => {
+  return (
+    <div className="m-5 w-72 h-96 card glass lg:card text-neutral-content">
+      <figure className="p-0">
+        <Image
+          alt="testimonial"
+          className="shadow-lg"
+          height={340}
+          layout="responsive"
+          src={image}
+          width={500}
+        />
+      </figure>
+      <div className="max-w-md card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>{content}</p>
+      </div>
+    </div>
   );
 };
 
@@ -240,135 +231,30 @@ const LandingPageNav = ({ fadeInSpeed = '1.5s' }) => {
 
 const TestimonialSection = () => {
   return (
-    <section className="testimonial-section">
-      <div className="section-title">
+    <section className="flex flex-col items-center justify-center h-1/2 py-28">
+      <div className="text-gray-500 text-2xl font-semibold pb-28">
         <h2>{'See What People Are Saying'}</h2>
       </div>
-      <div className="cards">
-        <div className="testimonial-card">
-          <div className="card glass lg:card text-neutral-content">
-            <figure className="p-0">
-              <Image
-                alt="testimonial"
-                className="shadow-lg"
-                height={340}
-                layout="responsive"
-                src="/images/HealthWorkShop.jpg"
-                width={500}
-              />
-            </figure>
-            <div className="max-w-md card-body">
-              <h2 className="card-title">{'Lovely People'}</h2>
-              <p>{"We haven't met a more generous group of giving souls."}</p>
-            </div>
-          </div>
-        </div>
-        <div className="testimonial-card">
-          <div className="card glass lg:card text-neutral-content">
-            <figure className="p-0">
-              <Image
-                alt="testimonial"
-                className="shadow-lg"
-                height={340}
-                layout="responsive"
-                src="/images/Volunteer.jpg"
-                width={500}
-              />
-            </figure>
-            <div className="max-w-md card-body">
-              <h2 className="card-title">{'Very Happy'}</h2>
-              <p>
-                {"I couldn't be more pleased with the way I've been treated."}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="testimonial-card">
-          <div className="card glass lg:card text-neutral-content">
-            <figure className="p-0">
-              <Image
-                alt="testimonial"
-                className="shadow-lg"
-                height={340}
-                layout="responsive"
-                src="/images/Humanitarian.jpg"
-                width={500}
-              />
-            </figure>
-            <div className="max-w-md card-body">
-              <h2 className="card-title">{'They Care'}</h2>
-              <p>
-                {"I couldn't be more pleased with the way I've been treated."}
-              </p>
-            </div>
-          </div>
-        </div>
+      <div
+        className=" bg-gradient-to-r from-accent to-blue-400 shadow-md md:flex md:p-32 "
+        id="cards-section"
+      >
+        <TestimonialCard
+          content="We haven't met a more generous group of giving souls."
+          image="/images/HealthWorkShop.jpg"
+          title="Lovely People"
+        />
+        <TestimonialCard
+          content="I couldn't be more pleased with the way I've been treated."
+          image="/images/Volunteer.jpg"
+          title="Very Happy"
+        />
+        <TestimonialCard
+          content="I couldn't be more pleased with the way I've been treated."
+          image="/images/Humanitarian.jpg"
+          title="They Care"
+        />
       </div>
-      <style jsx>{`
-        .testimonial-section {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          height: 1300px;
-          padding: 10px;
-        }
-        .cards {
-          background: linear-gradient(
-            90deg,
-            rgba(91, 164, 167, 0.699) 0%,
-            rgba(206, 117, 66, 0.336) 100%
-          );
-          padding: 70px;
-          display: flex;
-          align-items: flex-end;
-          height: 600px;
-          box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2),
-            0 2px 3px rgba(0, 0, 0, 0.2);
-        }
-        .testimonial-card {
-          display: flex;
-          width: 350px;
-          height: 600px;
-          overflow: scroll;
-          margin: 20px;
-          border-radius: 10px;
-          box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-        }
-        .section-title {
-          color: #0d2529b4;
-          font-size: 2.5rem;
-          font-weight: 500;
-          padding-bottom: 200px;
-        }
-
-        @media (max-width: 768px) {
-          .testimonial-section {
-            height: auto;
-            padding: 100px 0;
-
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-          }
-          .cards {
-            flex-direction: column;
-            height: auto;
-            padding: 0;
-          }
-          .testimonial-card {
-            width: 100%;
-            height: 500px;
-            margin: 0;
-            border-radius: 0;
-            box-shadow: none;
-          }
-          .section-title {
-            font-size: 1.5rem;
-            padding-bottom: 50px;
-          }
-        }
-      `}</style>
     </section>
   );
 };
@@ -377,12 +263,7 @@ const TestimonialSection = () => {
 // Export TestimonialSection end.
 // -----------------------------------------------------------------------------------------------------------------------
 
-// -----------------------------------------------------------------------------------------------------------------------
-// Main Component for this page lives here.
-// -----------------------------------------------------------------------------------------------------------------------
-
 const LandingPage = () => {
-  const router = useRouter();
   return (
     <>
       <Head>
@@ -391,15 +272,10 @@ const LandingPage = () => {
         <link href="https://www.margaritahumanitarian.org/" rel="canonical" />
         <link href="/favicon.ico" rel="icon" />
       </Head>
-      <LandingPageNav fadeInSpeed="1.5s" />
-      <main className="body">
-        <LargeHeroSection
-          bgImage="/images/HotMealDay.jpg"
-          heroHeight="85vh"
-          opacity="20"
-        >
+      <LandingPageNav />
+      <main className="justify-center">
+        <LargeHeroSection bgImage="/images/HotMealDay.jpg" opacity="20">
           <LargeHeroContent
-            fadeInSpeed="0.8s"
             mainTextSize="lg"
             title="Feed A Family Today"
             titleSize="5xl"
@@ -410,24 +286,12 @@ const LandingPage = () => {
               }
             </p>
 
-            <button
-              className="btn btn-accent rounded-full shadow-lg"
-              onClick={() => router.push('/in-kind')}
-              type="button"
-            >
-              {'Help Today'}
-            </button>
+            <Link href="/in-kind">
+              <a className="btn btn-accent rounded-btn">{'Feed A Family'}</a>
+            </Link>
           </LargeHeroContent>
         </LargeHeroSection>
         <TestimonialSection />
-
-        <style jsx>{`
-          .body {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-          }
-        `}</style>
       </main>
       <Footer />
     </>
