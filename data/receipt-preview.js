28b8613851315b793316d5eda5fe3c0da82b2aef
sabrinacux <sa.cux.dev@gmail.com>
2021-09-22 15:47:47 +0200

    added receipt preview fake data js file

---
+++ data/receipt-preview.js
@@ -0,0 +1,31 @@
+export const receipt_preview = {
+  header: {
+    title: 'Margarita Humanitarian Foundation',
+    subtitle: 'Kindness first',
+    ein: '84-4323038',
+    address: {
+      street: '1543 E. Palmdale Blvd',
+      place: 'Suite E',
+      city: 'Palmdale, CA 93550',
+    },
+    phone: '661-526-5236',
+    email: 'thankyou@mhfcares.com',
+  },
+  donation: {
+    date: 'July 20, 2021',
+    value: '500',
+    payment: 'personal check',
+    disclaimer: 'I, the undersigned representative, declare (or certify, verify or state) under penalty of perjury under the laws of United States of America that there were no goods or services provided as part of this donation.',
+    donor: {
+      name: 'John',
+      middlename: '',
+      surname: 'Smith',
+      phone: '661-555- 5555',
+      address: {
+        street: '1234 Main St.',
+        place: 'Suite 12',
+        city: 'Palmdale, CA 93550',
+      },
+    },
+  },
+};
