413e952af953e64a0b4ce266c427c2a198c7fd18
Sahil-101 <sahilflash123@gmail.com>
2021-10-04 15:29:29 +0530

    removed empty tags

--- components/PrimaryLayout.js
+++ components/PrimaryLayout.js
@@ -8,26 +8,24 @@ import Navbar from './Navbar';
 
 export default function PrimaryLayout({ children }) {
   return (
-    <>
-      <div className="flex flex-col min-h-screen">
-        <Head>
-          <title>{'Help a Family in Need'}</title>
-          <link href="/favicon.ico" rel="icon" />
-        </Head>
-        <Navbar />
-        <main className="flex-grow">
-          <HeroSection />
-          <div className="text-center hero-content md:m-auto">
-            <div className="w-lg">
-              <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 md:gap-x-8 gap-y-5 justify-center align-center content-center">
-                {children}
-              </div>
+    <div className="flex flex-col min-h-screen">
+      <Head>
+        <title>{'Help a Family in Need'}</title>
+        <link href="/favicon.ico" rel="icon" />
+      </Head>
+      <Navbar />
+      <main className="flex-grow">
+        <HeroSection />
+        <div className="text-center hero-content md:m-auto">
+          <div className="w-lg">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 md:gap-x-8 gap-y-5 justify-center align-center content-center">
+              {children}
             </div>
           </div>
-        </main>
-        <Footer />
-      </div>
-    </>
+        </div>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
