c2a0c27063daab7e029d0633395e3c1e395b3adb
Oleg Petrov <petrov.oleg.s@gmail.com>
2021-10-06 13:19:00 +0300

    💄 Prettify mobile menu styles

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { Routes } from '../models/routes';
 
 function SideNavbar({ show, setShow }) {
+  const linkClasses = 'btn btn-ghost btn-sm rounded-btn shadow-none';
+
+  const hideMenu = () => setShow(false);
+
   return (
     <div
       className={`${
@@ -10,31 +14,24 @@ function SideNavbar({ show, setShow }) {
       } fixed h-screen bg-neutral text-neutral-content transform top-0 left-0 w-64 overflow-auto ease-in-out transition-all duration-300 z-30  lg:hidden`}
     >
       <div className="flex-1 justify-center px-2 mx-2 pt-20 ">
-        <div
-          className="flex flex-col item-start"
-          onClick={() => setShow(false)}
-        >
-          <Link href={Routes.Home}>
-            <a className="btn btn-ghost btn-sm rounded-btn">{'Home'}</a>
+        <div className="flex flex-col item-start" onClick={hideMenu}>
+          <Link href="/">
+            <a className={linkClasses}>{'Home'}</a>
           </Link>
           <Link href={Routes.InKind}>
-            <a className="btn btn-ghost btn-sm rounded-btn">{'In-Kind'}</a>
+            <a className={linkClasses}>{'In-Kind'}</a>
           </Link>
           <Link href={Routes.Fund}>
-            <a className="btn btn-ghost btn-sm rounded-btn">{'Fund'}</a>
+            <a className={linkClasses}>{'Fund'}</a>
           </Link>
           <Link href={Routes.GiveYourTime}>
-            <a className="btn btn-ghost btn-sm rounded-btn">
-              {'Give Your Time'}
-            </a>
+            <a className={linkClasses}>{'Give Your Time'}</a>
           </Link>
           <Link href={Routes.PartnerWithUs}>
-            <a className="btn btn-ghost btn-sm rounded-btn">
-              {'Partner With Us'}
-            </a>
+            <a className={linkClasses}>{'Partner With Us'}</a>
           </Link>
         </div>
-        <div className="absolute bottom-0 left-0 w-full text-center">
+        <div className="absolute bottom-0 left-0 w-full text-center mb-2">
           {'Help Families in Need'}
         </div>
       </div>
