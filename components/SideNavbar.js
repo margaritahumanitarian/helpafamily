5dc483a4702ceb9cec511da051e608137cd26835
Oleg Petrov <petrov.oleg.s@gmail.com>
2021-10-06 22:36:57 +0300

    Refactored CSS classes

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { Routes } from '../models/routes';
 
 function SideNavbar({ show, setShow }) {
-  const linkClasses = 'btn btn-ghost btn-sm rounded-btn shadow-none';
-
   const hideMenu = () => setShow(false);
 
   return (
@@ -16,19 +14,19 @@ function SideNavbar({ show, setShow }) {
       <div className="flex-1 justify-center px-2 mx-2 pt-20 ">
         <div className="flex flex-col item-start" onClick={hideMenu}>
           <Link href="/">
-            <a className={linkClasses}>{'Home'}</a>
+            <a className="side-nav-btn">{'Home'}</a>
           </Link>
           <Link href={Routes.InKind}>
-            <a className={linkClasses}>{'In-Kind'}</a>
+            <a className="side-nav-btn">{'In-Kind'}</a>
           </Link>
           <Link href={Routes.Fund}>
-            <a className={linkClasses}>{'Fund'}</a>
+            <a className="side-nav-btn">{'Fund'}</a>
           </Link>
           <Link href={Routes.GiveYourTime}>
-            <a className={linkClasses}>{'Give Your Time'}</a>
+            <a className="side-nav-btn">{'Give Your Time'}</a>
           </Link>
           <Link href={Routes.PartnerWithUs}>
-            <a className={linkClasses}>{'Partner With Us'}</a>
+            <a className="side-nav-btn">{'Partner With Us'}</a>
           </Link>
         </div>
         <div className="absolute bottom-0 left-0 w-full text-center mb-2">
