3a1a4351279f878c37565ed6a0c5699abd95ac5e
ozer619 <prathameshd419@gmail.com>
2021-10-11 13:08:13 +0530

    changed to getServerSideProps

--- pages/landing-page.js
+++ pages/landing-page.js
@@ -17,7 +17,7 @@ import { createClient } from 'contentful';
  */
 // -------------------------------------------------------------------------------
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const client = createClient({
     environment: process.env.CONTENTFUL_ENVIRONMENT,
     space: process.env.CONTENTFUL_SPACE_ID,
