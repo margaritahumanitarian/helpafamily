6ea3edcd07b3d88fca2bcdd580e6753c813300f6
Sahil-101 <sahilflash123@gmail.com>
2021-10-20 20:11:26 +0530

    Added prototype for EmailCaptureComponent
    Co-authored-by: Prathamesh Desai <prathameshd419@gmail.com>

---
+++ components/EmailCapture.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function EmailCapture() {
+  console.log('What');
+  return (
+    <>
+      <div className="emailCaptureBox">{'EMAIL US ATT!!!!'}</div>
+    </>
+  );
+}
