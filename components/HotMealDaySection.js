874857147e1e0658d59fe20837e873529f5e3c63
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2021-10-21 17:23:40 -0700

    re working section

--- components/HotMealDaySection.js
+++ components/HotMealDaySection.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 function HotMealDaySection() {
   return (
-    <div className="text-center md:m-auto pb-20 hot-meal-day">
+    <div className="text-center md:mx-10 pb-20 hot-meal-day">
       <div className="w-lg p-5 pb-10 section-shadow">
         <div className="grid sm:grid-cols-1 md:grid-cols-2">
-          <div className="space-y-3 p-5 pr-10 md:text-left">
-            <h2 className="text-3xl md:text-5xl md:text-left font-bold">
+          <div className="space-y-3 p-14 pr-10 md:text-left">
+            <h2 className="text-3xl md:text-5xl md:text-left pb-7 font-bold">
               {'Hot Meal Day'}
             </h2>
             <p>
@@ -15,15 +15,10 @@ function HotMealDaySection() {
                 'Buy ingredients for 1 hot meal for 60 hungry people. Sample meals include spaghetti with Texas toast and caesar salad, chicken alfredo, enchilada taquitos. Served as take-home meal boxes at Grace Resources in Lancaster, California.'
               }
             </p>
-            <p>
-              {
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac volutpat diam, vel dignissim turpis. Aenean euismod ipsum a ipsum pellentesque sollicitudin. Phasellus lobortis libero in pellentesque posuere. Nulla est diam, sodales porttitor diam sed, viverra dapibus massa. Proin rhoncus a est nec aliquam. Fusce ultricies purus ultrices mi placerat interdum. Proin dignissim felis a sem iaculis sollicitudin. Donec blandit ex eu arcu consectetur feugiat. Nullam mollis felis in erat mollis finibus sed eu orci. Sed varius gravida posuere. Aliquam eu velit non ante ultrices consectetur.'
-              }
-            </p>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 md:items-end">
               <p className="md:col-span-2">
                 {
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac volutpat diam, vel dignissim turpis.'
+                  'Help us by donating your time or money. A small donation can help us bring hot meals to the most needy in our society.'
                 }
               </p>
               <a
@@ -31,7 +26,7 @@ function HotMealDaySection() {
                 href="https://www.margaritahumanitarian.org/upcoming-events#h.3f0nxu3620tm"
               >
                 <button
-                  className="btn btn-accent rounded-btn mt-5"
+                  className="btn btn-accent rounded-sm mt-5"
                   type="button"
                 >
                   {'Read More'}
@@ -54,9 +49,9 @@ function HotMealDaySection() {
           min-height: 60vh;
         }
         .section-shadow {
-          box-shadow: rgba(14, 30, 37, 0.1) 0px 0px 4px 0px,
-            rgba(14, 30, 37, 0.15) 0px 0px 16px 0px;
-          border-radius: var(--rounded-box, 1rem);
+          box-shadow: rgba(14, 30, 37, 0.082) 4px 4px 4px 0px,
+            rgba(14, 30, 37, 0.096) 4px 4px 12px 0px;
+          /* border-radius: var(--rounded-box, 1rem); */
         }
       `}</style>
     </div>
