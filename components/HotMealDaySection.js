7f58070094ace08dd8b9507aeeb216fd00fac1c5
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2021-10-21 19:09:05 -0700

    adjusted buttons for responsiveness

--- components/HotMealDaySection.js
+++ components/HotMealDaySection.js
@@ -1,7 +1,17 @@
+// import { CardAction } from './Card';
 import Image from 'next/image';
 import React from 'react';
+import useStripeSession from '../hooks/useStripeSession';
 
 function HotMealDaySection() {
+  const [handleSubmit, isPending] = useStripeSession();
+
+  const actionCost = 10;
+  const handleOnClick = () =>
+    handleSubmit({
+      amount: actionCost,
+      cause: 'Hot Meal Day.',
+    });
   return (
     <div className=" md:mx-10 pb-20 hot-meal-day">
       <div className="w-lg lg:p-5 section-box">
@@ -16,23 +26,33 @@ function HotMealDaySection() {
                   'Buy ingredients for 1 hot meal for 60 hungry people. Sample meals include spaghetti with Texas toast and caesar salad, chicken alfredo, enchilada taquitos. Served as take-home meal boxes at Grace Resources in Lancaster, California.'
                 }
               </p>
-              <div className="grid md:grid-cols-2 lg:grid-cols-3 md:items-end pt-5">
-                <p className="md:col-span-2">
+              <div className="lg:flex md:items-end pt-5">
+                <p className="md:col-span-2 pb-5 lg:pb-0">
                   {
-                    'Help us by donating your time or money. A small donation can help us bring hot meals to the most needy in our society.'
+                    'Make a donation today and you can help us bring hot meals to the most needy in our society.'
                   }
                 </p>
-                <a
-                  className="lg:ml-auto"
-                  href="https://www.margaritahumanitarian.org/upcoming-events#h.3f0nxu3620tm"
-                >
+                <div className="flex-buttons">
                   <button
-                    className="btn btn-accent rounded-sm mt-5"
+                    className="btn btn-accent rounded-sm mt-5 btn-size"
+                    disabled={isPending ? true : false}
+                    onClick={handleOnClick}
                     type="button"
                   >
-                    {'Read More'}
+                    {'Donate'}
                   </button>
-                </a>
+                  <a
+                    className="lg:ml-auto"
+                    href="https://www.margaritahumanitarian.org/upcoming-events#h.3f0nxu3620tm"
+                  >
+                    <button
+                      className="btn rounded-sm mt-5 btn-size"
+                      type="button"
+                    >
+                      {'Read More'}
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
@@ -47,6 +67,19 @@ function HotMealDaySection() {
         </div>
       </div>
       <style jsx>{`
+        .btn-size {
+          display: flex;
+          margin: 0 5px;
+          overflow-wrap: break-word;
+          word-wrap: break-word;
+          hyphens: auto;
+          white-space: nowrap;
+        }
+        .flex-buttons {
+          display: flex;
+          width: 100%;
+          margin-left: 3px;
+        }
         .hot-meal-day {
           min-height: 60vh;
           display: flex;
