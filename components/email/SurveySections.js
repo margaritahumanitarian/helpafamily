dcea87c7a13cef2d02a4ff08b89737cac84e2019
Sahil-101 <sahilflash123@gmail.com>
2021-10-22 20:39:17 +0530

    Completed Basid Layout!
    Co-authored-by: Prathamesh Desai <prathameshd419@gmail.com>

--- components/email/SurveySections.js
+++ components/email/SurveySections.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 export default function SurveySection() {
-  const SurveyContent = [
+  const surveyContentArray = [
     'How did you hear about us?',
     "What are causes you're interested in?",
     'How might would you like to help out',
@@ -9,18 +9,15 @@ export default function SurveySection() {
   ];
 
   const [surveyPointer, setSurveyPointer] = useState(0);
-  const [surveyContent, setSurveyContent] = useState(
-    SurveyContent[surveyPointer]
-  );
+  console.log(surveyPointer);
   const [input, setInput] = useState('');
 
   const handleSurveyButton = () => {
-    if (surveyPointer == 4) {
+    if (surveyPointer == 3) {
       return;
     }
     setSurveyPointer(surveyPointer + 1);
     console.log(surveyPointer);
-    setSurveyContent(SurveyContent[surveyPointer]);
     setInput('');
   };
 
@@ -29,25 +26,35 @@ export default function SurveySection() {
       <div className="email-captureBox border-4 text-center hero-content mx-auto my-10">
         <div>
           <div className="email-content-box pb-10 text-center hero-content md:m-auto">
-            <div className="description text-lg">{surveyContent}</div>
-            <div className="enter-email">
-              <div className="input-email">
-                <input
-                  className="mx-5 py-2 border-4 text-black"
-                  onChange={(value) => setInput(value.target.value)}
-                  value={input}
-                />
-              </div>
-              <div>
-                <button
-                  className="btn btn-accent mt-5"
-                  onClick={handleSurveyButton}
-                  type="submit"
-                >
-                  {surveyPointer < 4 ? 'Next' : 'Submit'}
-                </button>
-              </div>
+            <div
+              className={`description ${
+                surveyPointer < 3 ? 'text-lg' : 'text-4xl'
+              }`}
+            >
+              {surveyContentArray[surveyPointer]}
             </div>
+            {surveyPointer < 3 ? (
+              <div className="enter-email">
+                <div className="input-email">
+                  <input
+                    className="mx-5 py-2 border-4 text-black"
+                    onChange={(value) => setInput(value.target.value)}
+                    value={input}
+                  />
+                </div>
+                <div>
+                  <button
+                    className="btn btn-accent mt-5"
+                    onClick={handleSurveyButton}
+                    type="submit"
+                  >
+                    {surveyPointer < 2 ? 'Next' : 'Submit'}
+                  </button>
+                </div>
+              </div>
+            ) : (
+              ''
+            )}
           </div>
         </div>
       </div>
