ede5d983d59d152e0cd9a88c80d3e70ae097003e
Shamseera Kurumbeth <shamseerakurumbeth@thoughtworks.com>
2021-10-25 16:18:32 +1100

    deep source fix-issue-JS-0473

--- components/HygieneKitsCard.js
+++ components/HygieneKitsCard.js
@@ -9,32 +9,39 @@ import Card, {
 
 function HygieneKitsCard() {
   return (
-    <Card
-      backgroundImageAltText="Card about Hygiene Kits"
-      backgroundImageSource="/images/HygieneKit.webp"
-    >
-      <CardTitle>{'Hygiene Kits'}</CardTitle>
-      <CardParagraph>
-        {
-          'Order a bulk pack of hygiene kits for us to share with people experiencing homelessness.'
-        }
-      </CardParagraph>
-      <CardParagraph>
-        {
-          'Each hygiene kit allows an unhoused individual to freshen up and feel more confident about their hygiene for work, school, or even just to have a better day.'
-        }
-      </CardParagraph>
-      <CardAction
-        linkTo="https://www.amazon.com/96-Kits-Hygiene-Travel-Charity/dp/B0881YKMBL"
-        style="mb-4"
+    <>
+      <Card
+        backgroundImageAltText="Card about Hygiene Kits"
+        backgroundImageSource="/images/HygieneKit.webp"
       >
-        {'96 Hygiene Kits for $153'}
-      </CardAction>
+        <CardTitle>{'Hygiene Kits'}</CardTitle>
+        <CardParagraph>
+          {
+            'Order a bulk pack of hygiene kits for us to share with people experiencing homelessness.'
+          }
+        </CardParagraph>
+        <CardParagraph>
+          {
+            'Each hygiene kit allows an unhoused individual to freshen up and feel more confident about their hygiene for work, school, or even just to have a better day.'
+          }
+        </CardParagraph>
+        <CardAction
+          className="cardAction"
+          linkTo="https://www.amazon.com/96-Kits-Hygiene-Travel-Charity/dp/B0881YKMBL"
+        >
+          {'96 Hygiene Kits for $153'}
+        </CardAction>
 
-      <CardAddress label="Ship the kits to:">
-        {'1543 E Palmdale Blvd, Ste E, Palmdale, CA 93550'}
-      </CardAddress>
-    </Card>
+        <CardAddress label="Ship the kits to:">
+          {'1543 E Palmdale Blvd, Ste E, Palmdale, CA 93550'}
+        </CardAddress>
+      </Card>
+      <style jsx>{`
+        .cardAction {
+          margin-bottom: 1rem;
+        }
+      `}</style>
+    </>
   );
 }
 
