4c54ecbfc3569ebdba6888f0255fd7d6ddfcfbff
michael.duncan <michael.duncan@thoughtworks.com>
2021-10-28 11:16:52 +1100

    Bug-Risk fixes - JS-0002 (avoid using console)

--- pages/landing-page.js
+++ pages/landing-page.js
@@ -37,7 +37,6 @@ export async function getStaticProps() {
 }
 
 const TestimonialSection = ({ testimonialCard }) => {
-  console.log('testiMonialCard', testimonialCard);
   return (
     <section className="flex flex-col items-center justify-center h-1/2 py-28">
       <div className="text-2xl font-semibold text-gray-500 pb-28">
