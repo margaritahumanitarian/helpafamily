11a1102a05e9200ace7219aef3366b6cc0b6fdd8
Prathamesh Desai <prathameshd419@gmail.com>
2021-11-09 23:58:59 +0530

    formspark survey section

--- components/email/SurveySections.js
+++ components/email/SurveySections.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Questions from './Questions';
+import { useFormspark } from '@formspark/use-formspark';
 
 export default function SurveySection({
   data,
   nextComponent,
   previousComponent,
+  formID,
 }) {
   const questions = [
     'How did you hear about us?',
@@ -13,13 +15,16 @@ export default function SurveySection({
   ];
 
   let Answers = new Array(questions.length).fill('');
-
+  const [submit, submitting] = useFormspark({
+    formId: formID,
+  });
   const [surveyPointer, setSurveyPointer] = useState(0);
   const [answers, setAnswers] = useState(Answers);
-
-  const submitData = () => {
-    console.log(answers, 'with email as', data.Email);
-    //backend todo
+  const Email = data.Email;
+  const submitData = async () => {
+    console.log(answers, 'with email as', Email);
+    const complete_data = { answers, Email };
+    await submit({ complete_data });
   };
 
   const SubmitCheck = () => {
@@ -69,7 +74,7 @@ export default function SurveySection({
         </button>
         <button
           className={`btn btn-accent mt-5`}
-          disabled={!answers[surveyPointer]}
+          disabled={submitting}
           onClick={handleSurveyButton}
           type="submit"
         >
