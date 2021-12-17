64406088dba281bcbea83a575ef2749d4f196b40
Akshit Arora <akshitarora9211@gmail.com>
2021-12-17 13:00:20 +0530

    add useIntersectionObserver hook

--- components/LargeInfoSection.js
+++ components/LargeInfoSection.js
@@ -1,38 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
+import React, { useRef } from 'react';
 import { useContextTheme } from './ThemeContext';
-import { useInView } from 'react-intersection-observer';
+import { useIntersectionObserver } from '../hooks/useIntersectionObserver';
 import useStripeSession from '../hooks/useStripeSession';
-
 function LargeInfoSection({ isLeftLayout }) {
-  const animation = useAnimation();
+  const ref = useRef(null);
   const [handleSubmit, isPending] = useStripeSession();
-  const { ref, inView } = useInView({ threshold: 0.4 });
   const { cardsBackgroundColor, theme } = useContextTheme();
 
-  useEffect(() => {
-    const isMobile = window.innerWidth < 768;
-    if (!isMobile) {
-      if (inView) {
-        animation.start({
-          x: 0,
-          opacity: 1,
-          transition: {
-            type: 'spring',
-            duration: 1,
-            bounce: 0,
-          },
-        });
-      } else {
-        animation.start({
-          x: isLeftLayout ? '-300px' : '300px',
-          opacity: 0,
-        });
-      }
-    }
-    return () => animation.stop();
-  }, [inView, animation, isLeftLayout]);
+  const { isIntersecting } = useIntersectionObserver(ref, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+  });
 
   const actionCost = 10;
   const handleOnClick = () =>
@@ -52,9 +32,8 @@ function LargeInfoSection({ isLeftLayout }) {
         }`}
         src="/images/large-info-section.png"
       />
-      <motion.div
-        animate={animation}
-        className={`relative md:absolute flex flex-col w-full lg:w-5/12 h-11/12 py-12 lg:bg-gradient-to-r items-left from-transparent ${
+      <div
+        className={`transition transform duration-700 opacity-0 translate-x-32 relative md:absolute flex flex-col w-full lg:w-5/12 h-11/12 py-12 lg:bg-gradient-to-r items-left from-transparent ${
           theme === 'dark' ? 'via-[#1A2525]' : 'via-white'
         } z-10 ${
           isLeftLayout
@@ -64,7 +43,13 @@ function LargeInfoSection({ isLeftLayout }) {
             : `pl-0 lg:pl-8 pr-0 lg:pr-28 lg:left-20 ${
                 theme === 'dark' ? 'from-[#1A2525]' : 'from-white'
               } to-transparent lg:text-left lg:items-start`
+        } ${
+          isIntersecting
+            ? 'translate-x-0 opacity-100'
+            : '-translate-x-32 opacity-0'
         }`}
+        // data-class-in="translate-x-0 opacity-100"
+        // data-class-out="-translate-x-32 opacity-0"
       >
         <p className="font-bold text-3xl">{'Laptops for Families'}</p>
         <hr
@@ -106,7 +91,7 @@ function LargeInfoSection({ isLeftLayout }) {
             </button>
           </a>
         </div>
-      </motion.div>
+      </div>
     </div>
   );
 }
---
+++ hooks/useIntersectionObserver.js
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from 'react';
+export function useIntersectionObserver(ref, options = {}) {
+  const [element, setElement] = useState(null);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const observer = useRef(null);
+
+  const cleanOb = () => {
+    if (observer.current) {
+      observer.current.disconnect();
+    }
+  };
+
+  useEffect(() => {
+    setElement(ref.current);
+  }, [ref]);
+
+  useEffect(() => {
+    if (!element) return;
+    cleanOb();
+    const ob = (observer.current = new IntersectionObserver(
+      (changes) => {
+        changes.forEach((change) => {
+          setIsIntersecting(change.isIntersecting);
+          //   if (change.isIntersecting) {
+          //     /*
+          //      * 'split' turns our data attribute string into an array, and the spread operator (three dots)
+          //      * deconstructs it into the format that we need for manipulating the element's 'classList'
+          //      */
+          //     change.target.classList.remove(
+          //       ...change.target.getAttribute('data-class-out').split(' ')
+          //     );
+          //     change.target.classList.add(
+          //       ...change.target.getAttribute('data-class-in').split(' ')
+          //     );
+          //   } else {
+          //     change.target.classList.remove(
+          //       ...change.target.getAttribute('data-class-in').split(' ')
+          //     );
+          //     change.target.classList.add(
+          //       ...change.target.getAttribute('data-class-out').split(' ')
+          //     );
+          //   }
+        });
+      },
+      { ...options }
+    ));
+    ob.observe(element);
+    return () => {
+      cleanOb();
+    };
+  }, [element, options]);
+
+  return { isIntersecting };
+}
