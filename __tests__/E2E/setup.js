a96fcd09fbe348a8ff1920a40ece8d7b9c27e677
Francisco Bastos <1191453@isep.ipp.pt>
2022-05-15 13:44:55 +0100

    Update on tests

--- __tests__/E2E/setup.js
+++ __tests__/E2E/setup.js
@@ -1,6 +1,12 @@
+// Porto, 2022
+// This code was made with love by Francisco Bastos (https://www.linkedin.com/in/francisco-bastos-031369160/)
+// for the curriculum unit of Comprehension and Evolution of Software at the
+// master's in Software Engineering at the Faculty of Engineering of the University of Porto.
+// Feel free to contact the author if any questions arise.
+//
 var assert = require('assert'),
   webdriver = require('selenium-webdriver');
-
+// This is just a simple test to see if selenium can communicate with chrome.
 describe('Verify setup with Google Search', function () {
   it('test case should work', function () {
     var driver = new webdriver.Builder()
